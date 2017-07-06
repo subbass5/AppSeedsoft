79f88501586a06de55c82e2a49a0d3634f2376e9
Dell3421 <dumtk1@gmail.com>
2017-07-06 08:17:26 +0700

    Initial commit

---
+++ app/src/main/java/seedsoft/apps/com/appseedsoft/MainActivity.java
@@ -0,0 +1,416 @@
+package seedsoft.apps.com.appseedsoft;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.DialogInterface.OnClickListener;
+import uk.co.alt236.bluetoothlelib.device.BluetoothLeDevice;
+import uk.co.alt236.bluetoothlelib.device.beacon.BeaconType;
+import uk.co.alt236.bluetoothlelib.device.beacon.BeaconUtils;
+import uk.co.alt236.bluetoothlelib.device.beacon.ibeacon.IBeaconDevice;
+
+public class MainActivity extends AppCompatActivity {
+    JSONObject obj;
+    private static final String LOG_TAG = "MainActivity";
+    private ProgressDialog progressDialog_connect;
+    private  int n = 0 ;
+
+    private BluetoothManager btManager;
+    private BluetoothAdapter btAdapter;
+    private Handler scanHandler = new Handler();
+    private int scan_interval_ms = 10000;
+    private boolean isScanning = false;
+    TextView txt_distance,txtDevicename;
+    EditText status;
+    Button btnOpen,btnOFF;
+
+    //=====================mqtt
+    private TextView textStateDoor1,textStateDoor2;
+    private static final String TAG = "MQTT_NAT";
+    private String subscribeTopic = "/device/door/id001/status/";
+    private String content      = "Hello CloudMQTT";
+    private int qos             = 1;
+//    private String broker       = "tcp://m10.cloudmqtt.com:17923";
+//    private String clientId     = MqttClient.generateClientId();
+//    private String username     = "jhrhggnz";
+//    private String password     = "Qm1oj5Uj7soQ";    6,20
+
+    private String broker       = "tcp://188.166.188.78:1883";
+    private String clientId     = MqttClient.generateClientId();
+    private String username     = "seedsoft";
+    private String password     = "seedsoft";
+    private String topicMobile  = "/device/mobile/";
+    private String topicMobile_becon = "/device/mobile_beacon/";
+    boolean st = true;
+    String deviceID = "001";
+
+
+    MqttAndroidClient mqttAndroidClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Seed Soft");
+        setTitleColor(getResources().getColor(R.color.bootstrap_brand_info));
+
+        progressDialog_connect =new ProgressDialog(this);
+        openConnect(n);
+
+        btnOpen = (Button) findViewById(R.id.btn_Open);
+        status = (EditText) findViewById(R.id.state);
+        status.setEnabled(false);
+
+        setupMQTT();
+        // init BLE
+        btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        scanHandler.post(scanRunnable);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                obj = new JSONObject();
+                try {
+                    obj.put("mobile_id","001");
+                    obj.put("device_id","001");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                publishMessage(topicMobile,obj.toString());
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_exit)
+        {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private Runnable scanRunnable = new Runnable()
+    {
+        @Override
+        public void run() {
+
+            if (isScanning)
+            {
+                if (btAdapter != null)
+                {
+                    btAdapter.stopLeScan(leScanCallback);
+                }
+            }
+            else
+            {
+                if (btAdapter != null)
+                {
+                    btAdapter.startLeScan(leScanCallback);
+                }
+            }
+            isScanning = !isScanning;
+
+            scanHandler.postDelayed(this, scan_interval_ms);
+
+        }
+    };
+
+    // ------------------------------------------------------------------------
+    // Inner classes
+    // ------------------------------------------------------------------------
+
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback()
+    {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord)
+        {
+            final BluetoothLeDevice devicele = new BluetoothLeDevice(device,rssi,scanRecord,System.currentTimeMillis());
+            String scan = null;
+            String strRSSI = String.valueOf(rssi);
+            String strMac = String.valueOf(device.getAddress());
+            int Rssi_int = Integer.parseInt(strRSSI.substring(1));
+            if(BeaconUtils.getBeaconType(devicele) == BeaconType.IBEACON ){
+                final IBeaconDevice iBeaconDevice = new IBeaconDevice(devicele);
+                int TxPower = iBeaconDevice.getCalibratedTxPower();
+                obj = new JSONObject();
+                try {
+                    obj.put("mobile_id",deviceID);
+                    obj.put("device_id","001");
+                    obj.put("rssi",""+strRSSI.substring(1));
+                    obj.put("mac_beacon_mobile",""+strMac.toLowerCase());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+//                Toast.makeText(MainActivity.this, ""+String.valueOf(obj), Toast.LENGTH_SHORT).show();
+                scan = iBeaconDevice.getName()+ "  TXPOWER = "+TxPower+" RSSI = "+rssi +
+                        "Distance = "+calculateDistance(TxPower,rssi)+"\n";
+
+//              Toast.makeText(MainActivity.this, "Tx "+TxPower, Toast.LENGTH_SHORT).show();
+//                    txt_distance.setText(""+calculateDistance(TxPower,rssi)+" m. ");
+                if (Rssi_int < 50 && TxPower == -59 && st == true){
+                    publishMessage(topicMobile_becon, String.valueOf(obj));
+                }
+//                Toast.makeText(MainActivity.this, ""+scan, Toast.LENGTH_SHORT).show();
+
+//                  Toast.makeText(MainActivity.this, "Door open", Toast.LENGTH_SHORT).show();
+
+//                Toast.makeText(MainActivity.this, iBeaconDevice.getName()+ "  TXPOWER = "+TxPower+" RSSI = "+rssi +
+//                        "Distance = "+calculateDistance(TxPower,rssi), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    };
+
+    protected float calculateDistance(float txPower, double rssi) {
+
+        if (rssi == 0) {
+            return -1.0f; // if we cannot determine distance, return -1.
+        }
+
+        double ratio = rssi * 1.0 / txPower;
+
+        if (ratio < 1.0) {
+            return (float) Math.pow(ratio, 10);
+        } else {
+            double accuracy = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+            return (float) accuracy;
+        }
+    }
+
+    public void setupMQTT(){
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(),broker,clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if (reconnect){
+                    openConnect(1);
+                    addToHistory(serverURI);
+                    subscribeToTopic();
+                }else {
+                    addToHistory("Connected to "+serverURI);
+                    Toast.makeText(MainActivity.this,"Connected to "+serverURI.substring(6,20), Toast.LENGTH_SHORT).show();
+                    openConnect(n+=1);
+                    publishMessage("/device/mobile/con/","connect from mobile id "+deviceID);
+                    status.setText("Close");
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                String msg = new String(message.getPayload());
+                addToHistory("Incoming message: " + new String(message.getPayload()));
+//                Toast.makeText(MainActivity.this, "Yo!!!!!!!!", Toast.LENGTH_SHORT).show();
+                setState(msg);
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        //setup mqtt
+        MqttConnectOptions option = new MqttConnectOptions();
+        option.setUserName(username);
+        option.setPassword(password.toCharArray());
+        option.setAutomaticReconnect(true);
+        option.setCleanSession(false);
+
+        try{
+            mqttAndroidClient.connect(option, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectBufferOption = new DisconnectedBufferOptions();
+                    disconnectBufferOption.setBufferEnabled(true);
+                    disconnectBufferOption.setBufferSize(100);
+                    disconnectBufferOption.setPersistBuffer(false);
+                    disconnectBufferOption.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectBufferOption);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to connect to "+ broker);
+                }
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+    private void addToHistory(String mainText){
+
+        Log.d(TAG,mainText);
+
+
+    }
+    public void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(subscribeTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscribeTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    String msg = new String(message.getPayload());
+                    setState(msg);
+                    System.out.println("Message12: " + topic + " : " +msg);
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String topic,String state){
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(state.getBytes());
+            mqttAndroidClient.publish(topic, message);
+            addToHistory("Message Published");
+            if(!mqttAndroidClient.isConnected()){
+                addToHistory(mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public void setState(String in){
+        int stated = new Integer(in);
+//        Toast.makeText(this, ""+in, Toast.LENGTH_SHORT).show();
+        if(stated==1){
+            Toast.makeText(MainActivity.this, "Door open", Toast.LENGTH_SHORT).show();
+            st = false;
+            status.setText("Open");
+        }else {
+            st = true;
+            Toast.makeText(MainActivity.this, "Door Close", Toast.LENGTH_SHORT).show();
+            status.setText("Close");
+        }
+
+    }
+    private void openConnect(int state){
+        if(state == 0){
+            progressDialog_connect.setTitle("Connect mqtt");
+            progressDialog_connect.setIcon(R.drawable.imslogo11);
+            progressDialog_connect.setMessage("");
+            progressDialog_connect.show();
+        }else if(state ==1){
+            progressDialog_connect.hide();
+        }else {
+            n = 3;
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!btAdapter.isEnabled()){
+            Intent enable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enable,1);
+        }
+//        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+//            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+//        }
+    }
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        AlertDialog.Builder dialog2 = new AlertDialog.Builder(MainActivity.this);
+        dialog2.setTitle("Exit");
+        dialog2.setIcon(R.drawable.imslogo11);
+        dialog2.setCancelable(true);
+        dialog2.setMessage("Do you want to exit?");
+        dialog2.setPositiveButton("Yes", new OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+                dialog.dismiss();
+            }
+        });
+        dialog2.setNegativeButton("No", new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog2.show();
+    }
+}
---
+++ app/src/main/java/seedsoft/apps/com/appseedsoft/Splash_Activity.java
@@ -0,0 +1,51 @@
+package seedsoft.apps.com.appseedsoft;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+/**
+ * Created by Dell3421 on 7/6/2017.
+ */
+
+public class Splash_Activity extends AppCompatActivity{
+    Handler handler;
+    Runnable runnable;
+    long delay_time;
+    long time = 1500L;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_layout);
+        getSupportActionBar().hide();
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Splash_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+    }
+    public void onResume() {
+        super.onResume();
+        delay_time = time;
+        handler.postDelayed(runnable, delay_time);
+        time = System.currentTimeMillis();
+    }
+
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delay_time - (System.currentTimeMillis() - time);
+    }
+}
